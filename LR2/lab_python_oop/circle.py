21437b8fd2da82b22ff0e35ebe02068ed49dd678
Alexey <lesh77906@gmail.com>
2020-09-15 12:37:30 +0300

    Реализован класс "Круг", который наследуется от класса "Геометрическая фигура". Метод находжения площади переопределен.

---
+++ LR2/lab_python_oop/circle.py
@@ -0,0 +1,18 @@
+from LR2.lab_python_oop.geomFigure import GeomFigure
+from LR2.lab_python_oop.figureColor import FigureColor
+import math
+
+class Circle(GeomFigure):
+
+    fig = "Круг"
+
+    def __init__(self, radius, color):
+        self.r = radius
+        self.figureColor = FigureColor()
+        self.figureColor.set(color)
+
+    def square(self):
+        return math.pi*pow(self.r, 2)
+
+    def about(self):
+        print("Фигура: {}, Радиус: {}, Площадь: {}, Цвет: {}".format(self.fig, self.r, self.square(), self.figureColor.get()))
