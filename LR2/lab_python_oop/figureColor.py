6f483f3e9db85c1a753ec849d17175d0095ed71a
Alexey <lesh77906@gmail.com>
2020-09-15 08:06:14 +0300

    Реализован класс "Цвет фигуры", описывающий свойство цвета геометрических фигур

---
+++ LR2/lab_python_oop/figureColor.py
@@ -0,0 +1,11 @@
+class figureColor:
+    def __init__(self):
+        self._color = None
+
+    def get(self):
+        return self._color
+
+    def set(self, value):
+        self._color = value
+
+
