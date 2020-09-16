b2250bd2e06b3d1dcccc85b4e25544dbb65e6f10
Alexey <lesh77906@gmail.com>
2020-09-16 13:00:07 +0300

    minor

--- LR2/main.py
+++ LR2/main.py
@@ -1,16 +1,20 @@
 import requests
-from LR2.lab_python_oop.rectangle import *
-from LR2.lab_python_oop.squad import *
-from LR2.lab_python_oop.circle import *
-
-print(requests.__license__ + "\n")
-
-rect = Rectangle(10,15, "Blue")
-sqd = Squad(10,"Black")
-circ = Circle(5, "Red")
-
-rect.about()
-sqd.about()
-circ.about()
+from LR2.lab_python_oop.rectangle import Rectangle
+from LR2.lab_python_oop.squad import Squad
+from LR2.lab_python_oop.circle import Circle
 
 
+def main():
+    print(requests.__license__ + "\n")
+
+    rect = Rectangle(10,15, "Blue")
+    sqd = Squad(10,"Black")
+    circ = Circle(5, "Red")
+
+    rect.about()
+    sqd.about()
+    circ.about()
+
+
+if __name__ == "__main__":
+    main()
