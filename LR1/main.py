aded0ccbb5b81dd7640a1ba4898728cfe6f3c54e
Alexey <lesh77906@gmail.com>
2020-09-26 16:03:49 +0300

    Комментирование

--- LR1/main.py
+++ LR1/main.py
@@ -5,19 +5,26 @@ import funcs
 
 print("\033[34mРешаем биквадратные уравнения \033[0m")
 
+# Переменная для выхода из цикла
 stop = 0
 
 parser = argparse.ArgumentParser()
+# Описываем аргументы командной строки (для -h)
 parser.add_argument("--a", help = "Коэффициент А Биквадратного уравнения", default=None, type=float)
 parser.add_argument("--b", help = "Коэффициент B Биквадратного уравнения", default=None, type=float)
 parser.add_argument("--c", help = "Коэффициент C Биквадратного уравнения", default=None, type=float)
+# Парсим аргументы
 args = parser.parse_args()
+# Логическая переменная первого прогона с аргументами
 firstRunWithArgs = False
 
+# Бесконечный цикл основной программы
 while stop == 0:
 
+    # Кортеж для проверенных введенных аргументов командной строки
     argsValues = funcs.checkArgs(args.a, args.b, args.c, firstRunWithArgs)
 
+    # Ветвление получения коэфициентов уравнения (либо из cmd, либо при в воде)
     if argsValues[3] == False:
         a = argsValues[0]
         b = argsValues[1]
@@ -30,13 +37,18 @@ while stop == 0:
 
     print(f"\n\033[33mУравнение:\033[0m {a}X^4 + {b}X^2 + {c} = 0\n")
 
+    # Проверка коэфициентов (возвращает -1, 0 или 1) и далее var stop обрабатывается в ветвлении
     stop = funcs.checkCoefficients(a, b, c)
 
     if stop == 0:
+        # если вернулся 0, значит с коэфициентами все нормально, для дальнейшего нахождения корней с вычислением
+        # дискриминанта
         stop = funcs.discriminant(a, b, c)
     if stop == -1:
+        # вернулся -1, значит надо ввести аргументы заного
         stop = 0
     elif stop == 1:
+        # предлагается завершить работу программы
         choice = ""
         while True:
             choice = input("\nХотите завершить? (y/n) ")
