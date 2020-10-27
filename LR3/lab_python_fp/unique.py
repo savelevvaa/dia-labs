2dc9e045efb271e2c084e1113fe1ff3264ded843
Alexey <lesh77906@gmail.com>
2020-10-27 16:09:12 +0300

    Исправлен итератор, реализована функция для проверки итератора

--- LR3/lab_python_fp/unique.py
+++ LR3/lab_python_fp/unique.py
@@ -1,60 +1,43 @@
-class Unique(object):
+class Unique():
     def __init__(self, items, **kwargs):
-        self.i = 0
-        for k, v in kwargs.items():
-            self.iCase = v
-            break
+        ignore = kwargs["ignore"] if "ignore" in kwargs else False
         self.uniqueItems = set()
-        self.temp = []
-        self.items = items
+        for i in items:
+            if isinstance(i, str) and ignore:
+                self.uniqueItems.add(i.lower())
+            else:
+                self.uniqueItems.add(i)
 
     def __next__(self):
-        if self.iCase == False:
-            if str(type(self.items)) == "<class 'generator'>":
-                print()
-                # self.genList = self.generator(self.items)
-                # todo доделать итератор (ошибка именно в нем!!!!)
-
-
-            for i in self.items:
-                if type(i) == str:
-                    self.temp.append(i.upper())
-                else:
-                    self.temp.append(i)
-        else:
-            self.temp = self.items
-        while True:
-            if self.i >= len(self.items):
-                raise StopIteration
-            else:
-                item = self.temp[self.i]
-                self.i = self.i + 1
-                if item not in self.uniqueItems:
-                    self.uniqueItems.add(item)
-                    return item
-
-    def generator(self, gen):
-        list = []
-        while True:
-            try:
-                list.append(gen.__next__())
-            except StopIteration:
-                break
-        return list
+        if len(self.uniqueItems) > 0:
+            return self.uniqueItems.pop()
+        raise StopIteration()
 
     def __iter__(self):
         return self
 
-# list1 = [1,2,3,4,4,3,2,4,5,3,6,7,8]
-# list2 = ['a','B','c','d','c','e','a','B','C','A','b']
-# list3 = ['a','B',1,'c','d',3,'c','e',3,'a','B',2,'C',5,'A',5,'b']
-# uniqueList = []
-#
-# for i in Unique(list3, ignore=True):
-#     uniqueList.append(i)
-#     print(i)
-#
-# print(uniqueList)
-# #print(sorted(uniqueList))
+def main():
+    print("\033[33mlist1\033[0m:        ", list1)
+    print("\033[36mUnique list1\033[0m: ", sorted(list(Unique(list1))), "\n")
+
+    print("\033[33mlist2\033[0m:        ", list2)
+    print("\033[36mUnique list2\033[0m: ", sorted(list(Unique(list2, ignore=False))), "\n")
+    print("\033[33mlist2\033[0m:                        ", list2)
+    print("\033[36mUnique list2\033[0m(\033[31mignore = True\033[0m):  ", sorted(list(Unique(list2, ignore=True))), "\n")
+
+    print("\033[33mlist3\033[0m:        ", list3)
+    print("\033[36mUnique list3\033[0m: ", list(Unique(list3, ignore=False)), "\n")
+    print("\033[33mlist3\033[0m:                        ", list3)
+    print("\033[36mUnique list3\033[0m(\033[31mignore = True\033[0m):  ", list(Unique(list3, ignore=True)),
+          "\n")
+
+
+list1 = [1,2,3,4,4,3,2,4,5,3,6,7,8]
+list2 = ['a','B','c','d','c','e','a','B','C','A','b']
+list3 = ['a','B',1,'c','d',3,'c','e',3,'a','B',2,'C',5,'A',5,'b']
+
+
+if __name__ == "__main__":
+    main()
 
 
