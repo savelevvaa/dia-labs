de664282cf91d4bdfaee78167cf06cc94413ef6d
Alexey <lesh77906@gmail.com>
2020-10-27 20:44:33 +0300

    done

--- LR3/process_file.py
+++ LR3/process_file.py
@@ -1,16 +1,12 @@
-from LR3.lab_python_fp import print_result, sort, unique, gen_random, field, cm_timer
+from LR3.lab_python_fp import print_result, unique, gen_random, field, cm_timer
 import json
-import sys
-# Сделаем другие необходимые импорты
 
 
 path = "/Users/dlnwlkmn/Desktop/dia-labs/LR3/data_light.json"
 
-# Необходимо в переменную path сохранить путь к файлу, который был передан при запуске сценария
+with open(path) as f, cm_timer.cm_timer_1():
+    data = json.load(f)
 
-# Далее необходимо реализовать все функции по заданию, заменив `raise NotImplemented`
-# Предполагается, что функции f1, f2, f3 будут реализованы в одну строку
-# В реализации функции f4 может быть до 3 строк
 
 @print_result.print_result
 def f1(arg):
@@ -31,17 +27,14 @@ def f3(arg):
 
 @print_result.print_result
 def f4(arg):
-    return list(i + f", зарплата {gen_random.gen_random(1,100000,200000)[0]} руб." for i in arg)
+    return list(i + f", зарплата \033[34m{gen_random.gen_random(1,100000,200000)[0]}\033[0m руб." for i in arg)
+
+
+def main():
+
+    with cm_timer.cm_timer_1():
+        f4(f3(f2(f1(data))))
 
 
 if __name__ == '__main__':
-
-    with open(path) as f, cm_timer.cm_timer_1():
-        data = json.load(f)
-
-    with cm_timer.cm_timer_1():
-        temp1 = f1(data)
-        temp2 = f2(temp1)
-        temp3 = f3(temp2)
-        temp4 = f4(temp3)
-        print()
+    main()
