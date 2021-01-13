0c86d45ec5778f8e6e0da6d86c93b4de9fce95f7
Alexey <lesh77906@gmail.com>
2021-01-13 20:01:34 +1100

    Изменение ссылок

--- DZ/DZApp/urls.py
+++ DZ/DZApp/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('<int:tab_index>/', views.details, name='details'),
     path('review/', views.review, name='review'),
-    path('review/waybill', views.review_waybill, name='review_waybill'),
-    path('review/partnership', views.review_partnership, name='review_partnership'),
+    path('review/<int:tab_index>/', views.review_details, name='review_details'),
 ]
