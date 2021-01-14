0da536b637180762165e3e5abe65860a2ee13cd7
Alexey <lesh77906@gmail.com>
2021-01-15 00:46:26 +1100

    Добавлена/изменена адресация

--- DZ/DZApp/urls.py
+++ DZ/DZApp/urls.py
@@ -3,12 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('<int:tab_index>/', views.details, name='details'),
+    path('', views.loginPage, name='login'),
+    path('register/', views.regPage, name='register'),
+    path('logout/', views.logoutPage, name='logout'),
+
+    path('index/', views.index, name='home'),
+    path('details/<int:tab_index>/', views.details, name='details'),
+    path(r'update/<int:tab_index>/<int:pk>$', views.update, name='update'),
+    path(r'delete/<int:tab_index>/<int:pk>$', views.delete, name='delete'),
     path('review/', views.review, name='review'),
     path('review/<int:tab_index>/', views.review_details, name='review_details'),
-
-    path('register/', views.regPage, name='register'),
-    path('login/', views.loginPage, name='login'),
-    path('logout/', views.logoutPage, name='logout'),
 ]
